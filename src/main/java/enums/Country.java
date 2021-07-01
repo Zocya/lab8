d746ff92c646afb6582d89cb59bf056c524c3eb4
Sofia Paska <Sofiia.Paska.KNM.2020@lpnu.ua>
2021-07-01 22:53:25 +0300

    added 8 lab

---
+++ src/main/java/enums/Country.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum Country {
+    ENGLAND,
+    CHINA,
+    FRANCE,
+    BRAZIL
+}
---
+++ src/main/java/models/Flash.java
@@ -0,0 +1,19 @@
+package models;
+
+import enums.Country;
+import enums.Purpose;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(callSuper = true)
+public class Flash extends Device{
+    private int power;
+    private String colour;
+    public Flash(String name, int price, Country country, int weight, int length, Purpose purpose, int height,
+    int power, String colour) {
+        super(name, price, country, weight, length, purpose, height);
+        this.power = power;
+        this.colour = colour;
+    }
+}
